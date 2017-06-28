6f32d6588273fbf80e389fffe4875a6922fa76c1
Oricul <Oricul@users.noreply.github.com>
2017-06-27 21:11:00 -0500

    Some error handling updates.

--- ori-self.py
+++ ori-self.py
@@ -21,7 +21,7 @@ bot = commands.Bot(command_prefix='~', description=description, self_bot=True)
 @bot.event
 async def on_command_error(ctx, error):
     print('1: {0} ||| {1}'.format(ctx,error))
-    print("2: {0} ||| {1)".format(error.message,error.view))
+    print("2: {0} ||| {1}".format(error.message,error.view))
     if isinstance(error, commands.NoPrivateMessage):
         await ctx.send(content='This command cannot be used in private messages.')
     elif isinstance(error, commands.DisabledCommand):
